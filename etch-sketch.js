c39db521dd04cc3beacb08c92d5fbd13fdbc3b25
Bright-Anyawe <157108952+Bright-Anyawe@users.noreply.github.com>
2024-03-12 08:12:43 +0000

    Add Eventlistener for hover effect

--- etch-sketch.js
+++ etch-sketch.js
@@ -4,8 +4,15 @@ for (i = 0; i < 16 * 16; i++) {
     const cell = document.createElement('div');
     cell.classList.add('cell')
 
+    cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = getRandomColor();
+    });
+
     gridContainer.appendChild(cell);
+
+
 }
+
 
 
 
